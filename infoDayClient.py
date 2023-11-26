28e53c01e079f45a2aa02c1be8855665e9bca102
KiuQ LI <78521221+Kiu-Q@users.noreply.github.com>
2023-11-26 23:38:31 +0800

    dual init

---
+++ infoDayClient.py
@@ -0,0 +1,194 @@
+import pygame as pg
+import random
+import mediapipe as mp
+import cv2
+import time
+import serial
+
+AMD = 0
+LIMIT = 45
+PORT = "COM5"
+
+pg.init()
+
+w, h = pg.display.Info().current_w-30, pg.display.Info().current_h-100
+screen = pg.display.set_mode((w,h))
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+font = pg.font.SysFont("Arial", 30)
+
+pg.display.set_caption("Ho Fung College Info Day Shooting Game - Christmas Cookies DUAL PLAYER")
+
+file = "src/file"
+
+player_image = pg.transform.scale(pg.image.load("assets/player.png"), (w//5, w//5))
+bgs = [pg.transform.scale(pg.image.load("assets/bg/%d.png"%i), (w+30, h+100)) for i in range(3)]
+enemys = [pg.transform.scale(pg.image.load("assets/enemy/%d.png"%i), (w//10, w//10*pg.image.load("assets/enemy/%d.png"%i).get_height()//pg.image.load("assets/enemy/%d.png"%i).get_width())) for i in range(13)]
+loads = [pg.transform.scale(pg.image.load("assets/load/%d.png"%i), (w//2, w//2)) for i in range(16)]
+gMans = [pg.transform.scale(pg.image.load("assets/gMan/%d.png"%i), (w//10, w//10*pg.image.load("assets/gMan/%d.png"%i).get_height()//pg.image.load("assets/gMan/%d.png"%i).get_width())) for i in range(4)]
+loadings = [pg.transform.scale(pg.image.load("assets/loading/%d.png"%i), (w//5, w//5*pg.image.load("assets/loading/%d.png"%i).get_height()//pg.image.load("assets/loading/%d.png"%i).get_width())) for i in range(4)]
+carck = pg.mixer.Sound(file="assets/sound.wav")
+
+class Player:
+    def __init__(self):
+        self.pos = [w//2, h//2]
+        self.rect = player_image.get_rect()
+        self.rect.center = self.pos
+        
+    def update(self, fPos):
+        self.pos = fPos
+        self.rect.center = fPos
+        
+    def draw(self):
+        screen.blit(player_image, self.rect)
+
+class Enemy:
+    def __init__(self):
+        self.pic = enemys[random.randint(0, 12)]
+        self.pos = pos[score]
+        self.rect = self.pic.get_rect()
+        self.rect.center = self.pos
+        
+    def draw(self):
+        screen.blit(self.pic, self.rect)
+        
+    def collide(self, pos):
+        return self.rect.collidepoint(pos)
+
+def printText(text, color = BLACK, add = 0):
+    for i in text:
+        temp = font.render(i, True, color)
+        screen.blit(temp, (w // 2 - temp.get_width()//2, h // 3 +(text.index(i)+1)*30+add))
+        pg.display.update()
+        
+def Loading(limit = 50):
+    cnt = 0
+    while cnt<=limit:
+        screen.fill(WHITE)
+        screen.blit(loads[cnt%len(loads)], (w//2-loads[cnt%len(loads)].get_width()//2, h//2-loads[cnt%len(loads)].get_height()//2))
+        screen.blit(gMans[cnt%len(gMans)], (w//2-gMans[cnt%len(gMans)].get_width()//2, h//2-gMans[cnt%len(gMans)].get_height()//2))
+        screen.blit(loadings[cnt%len(loadings)], (w//2-loadings[cnt%len(loadings)].get_width()//2, h//3-loadings[cnt%len(loadings)].get_height()//2))
+        pg.display.update()
+        cnt += 1
+        time.sleep(0.01)
+       
+connect = False
+while not connect:
+    try:
+        ser = serial.Serial(PORT, 9600)
+        connect = True
+    except Exception as ex:
+        connect = False
+    time.sleep(0.1)
+
+q = False
+while not q:
+            
+    screen.blit(bgs[0], (0,0))
+    printText(["HFC Info Day Hand Detect Shooting Game - Christmas Cookies DUAL PLAYER",
+               "Welcome, press <SPACE> to start", 
+               "Connetion Status:", 
+               "Connected"])
+            
+    press = False
+    while not press:
+        event = pg.event.wait()            
+        if event.type == pg.KEYDOWN and event.key == pg.K_q:
+            q, press = True, True
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            press = True
+            #ser.write("s")
+
+    if q:
+        break
+
+    Loading(20)
+    
+    score = 0
+    times = LIMIT
+    pos = [(random.randint(w//5, w-w//10), random.randint(h//5, h-h//10)) for i in range(LIMIT*2)]
+
+    mpHands = mp.solutions.hands
+    cap = cv2.VideoCapture(0)
+    
+    for i in pos:
+        for j in i:
+            #ser.write(j)
+            print(str(j))
+    
+    player = Player()
+    enemy = Enemy()
+
+    with mpHands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        
+        start = pg.time.get_ticks()
+        run = True
+              
+        Loading(20)
+                
+        while run:
+            screen.fill(WHITE)
+            screen.blit(bgs[1], (0,0))
+            
+            player.draw()
+            enemy.draw()
+            
+            remaining = times - (pg.time.get_ticks() - start) // 1000
+            
+            screen.blit(font.render("Score: " + str(score), True, BLACK), (10, 10))
+            screen.blit(font.render("Time Remaining: " + str(remaining), True, BLACK), (10, 40))
+
+            results = hands.process(cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB))
+        
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    player.update([w-(hand_landmarks.landmark[7].x * w), hand_landmarks.landmark[7].y * h+AMD])
+                    if enemy.collide(player.pos):
+                        carck.play()
+                        enemy = Enemy()
+                        score += 1
+            else:
+                pass
+
+            if remaining <= 0:
+                run = False
+
+            pg.display.update()
+    
+    Loading(20)
+    
+    resopnse = False
+    while not resopnse:
+        try:
+            #cScore = int(ser.read())
+            cScore = 20
+            resopnse = True
+        except Exception as ex:
+            cScore = 0
+        time.sleep(0.1)
+
+    if score >= cScore:
+        win = True
+        #ser.write("lose")
+        print("lose")
+    else:
+        win = False
+        #ser.write("win")
+        print("win")
+        
+    Loading(20)
+    
+    screen.blit(bgs[2], (0,0))
+    
+    printText([("Time's Up! Final Score: %d, you "%score) + ("win!" if win else "lose!"),
+               "Press <SPACE> to restart"], WHITE, 60)
+
+    press = False
+    while not press:
+        event = pg.event.wait()            
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            press = True
+            Loading()
